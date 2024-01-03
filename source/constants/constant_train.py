9696c3ab6eb85b772096577397e3ce9f26448ffa
amruta2503 <142072684+amruta2503@users.noreply.github.com>
2024-01-02 18:04:53 -0800

    entity config

---
+++ source/constants/constant_train.py
@@ -0,0 +1,16 @@
+TARGET_COLUMN = "SalePrice"
+PIPELINE_NAME = "training pipeline"
+ARTIFACT_DIR = "artifact"
+FILE_NAME = "training_data.csv"
+
+TRAIN_FILE_NAME = "train.csv"
+TEST_FILE_NAME = "test.csv"
+
+MONGODB_URL_KEY = "mongodb+srv://amrutasharnangat2503:mongo1234@cluster0.r6fy3lw.mongodb.net/?retryWrites=true&w=majority"
+DATABASE_NAME = "db-house-saleprice"
+
+DI_COLLECTION_NAME = 'house-price'
+DI_DIR_NAME = "data_ingestion"
+DI_FEATURE_STORE_DIR ="feature_store"
+DI_INGESTED_DIR = "ingested"
+DI_TRAIN_TEST_SPLIT_RATIO = 0.2
